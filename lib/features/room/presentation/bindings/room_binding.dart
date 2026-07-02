4e51c89b780b55409effafa8f97cc57736d88c79
Mr.Arvind <arvindmr4818@gmail.com>
2026-07-01 20:43:53 -0700

    feat: add missing room_binding.dart

--- lib/features/room/presentation/bindings/room_binding.dart
+++ lib/features/room/presentation/bindings/room_binding.dart
@@ -1,18 +1,11 @@
-// ═══════════════════════════════════════════════════════════════════════════
-// FILE: lib/features/room/bindings/room_binding.dart
-// ARVIND PARTY - ROOM BINDING
-// ═══════════════════════════════════════════════════════════════════════════
-
 import 'package:get/get.dart';
 import '../controllers/room_controller.dart';
+import '../../services/room_service.dart';
 
 class RoomBinding extends Bindings {
-  final String roomId;
-  final String roomOwnerId;
-  RoomBinding({this.roomId = '', this.roomOwnerId = ''});
-
   @override
   void dependencies() {
-    Get.lazyPut<RoomController>(() => RoomController(roomId: roomId, roomOwnerId: roomOwnerId));
+    Get.lazyPut<RoomService>(() => RoomService());
+    Get.lazyPut<RoomController>(() => RoomController());
   }
 }
